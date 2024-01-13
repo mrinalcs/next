07efd6627460cde2c9b9616f78b6340a530dbcc1
Mrinal Chandra Sarkar <mrinalvbu@gmail.com>
2024-01-13 05:57:54 +0000

    remove image

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Image from 'next/image'
 
-import avatar from '@/app/mrinal.webp';
-
 
 export default function Home() {
   return (
     <main>
       <article>
-      <Image
-      className='my-photo'
-      src={avatar}
-      width={120}
-      height={200}
-      alt="Picture of the author"
-      />
+      
         <h1>Mrinal Chandra Sarkar</h1>
         <p>Student of M.Sc in Statistics at Visva Bharati, Shantiniketan.</p>
 
